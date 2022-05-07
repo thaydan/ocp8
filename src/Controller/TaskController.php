3a5bbcfb8be521757439f095b9fa035130a15cc5
Romain <contact@rominfo.fr>
2022-05-07 17:32:13 +0200

    enhancement

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -107,7 +107,12 @@ class TaskController extends AbstractController
         $task->toggle(!$task->isDone());
         $this->manager->flush();
 
-        $this->addFlash('success', sprintf('La tâche %s a bien été marquée comme faite.', $task->getTitle()));
+        if ($task->isDone()) {
+            $this->addFlash('success', sprintf('La tâche %s a bien été marquée comme faite.', $task->getTitle()));
+        }
+        else {
+            $this->addFlash('success', sprintf('La tâche %s a bien été marquée comme non terminée.', $task->getTitle()));
+        }
 
         return $this->redirectToRoute('task_list');
     }
